a316e3073817149807f9d7df9f1d4a2ad606f892
jaewonyoo99 <jaewonyoo@ordercheck.io>
2023-03-09 23:49:35 +0900

    feat(useMobile): Add useMobile hook

---
+++ src/hooks/useMobile.ts
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import isMobile from "is-mobile";
+
+export const useMobile = () => {
+  const [mobile, setMobile] = React.useState(false);
+  React.useEffect(() => {
+    if (isMobile()) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }, []);
+  return mobile;
+};
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import MobileNavbar from "src/components/organisms/Navbar/Mobile";
 import RootTemplate from "src/components/Template/RootTemplate";
-import mobile from "is-mobile";
 import DesktopNavbar from "src/components/organisms/Navbar/Desktop";
+import { useMobile } from "src/hooks/useMobile";
 const Main = () => {
+  const mobile = useMobile();
   return (
-    <RootTemplate>
-      {mobile() ? <MobileNavbar /> : <DesktopNavbar />}
-    </RootTemplate>
+    <RootTemplate>{mobile ? <MobileNavbar /> : <DesktopNavbar />}</RootTemplate>
   );
 };
 
