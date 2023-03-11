ce0ee8bac10496f31339f847bb97c92f8602cab5
emulsion199 <wodnjs471@naver.com>
2023-03-11 14:33:32 +0900

    test

--- src/components/organisms/Navbar/Mobile/index.tsx
+++ src/components/organisms/Navbar/Mobile/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 const MobileNavbar = () => {
   return (
     <div className="bg-black">
-      <span className="text-white">{"모바일"}</span>
+      <span className="text-white">{"모바일."}</span>
     </div>
   );
 };
