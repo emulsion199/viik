b381f28d3d2091679869d0ae2e4794595005cb7c
emulsion199 <wodnjs471@naver.com>
2023-03-11 15:31:07 +0900

    feat(tailwind.config.js): Add transparent

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const COLORS = {
   white: "#FFF",
   black: "#101011",
+  transparent: "transparent",
   bg: {
     default: "#FAF1E5",
     hover: "#EAE4D8",
