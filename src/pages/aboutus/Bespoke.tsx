e2b4579ac75a26aae8cf19bf313b95cbc4b60fdd
ysyss <alsdbtjd0103@naver.com>
2023-03-28 19:06:46 +0900

    Feat: tailwind config add green color

--- src/pages/aboutus/Bespoke.tsx
+++ src/pages/aboutus/Bespoke.tsx
@@ -1,13 +1,15 @@
+import { useMobile } from '#hooks/useMobile';
 import React from 'react';
 
 const Bespoke = () => {
+   const mobile = useMobile();
    return (
       <div className='min-w-[375px] column center'>
          <span className='text-center text-p2 md:text-p3 font-medium mb-5'>
             나만의 비스포크를 찾아보세요.
             <br /> 더 깊이 나를 이해하고, 진정한 나를 발견하세요.
          </span>
-         <span className='text-center text-lg leading-[21px] font-light mb-20'>
+         <span className='text-center text-lg leading-[21px] font-light mb-20 text'>
             Discover your ideal Bespoke.
             <br /> Determine your Bespoke to understand <br />
             how your internal clock works.
