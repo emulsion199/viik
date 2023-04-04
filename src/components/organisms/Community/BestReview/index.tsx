f10bd6284c6d1485ab3fe466bc6d7a59d9bad665
ysyss <alsdbtjd0103@naver.com>
2023-04-04 18:09:21 +0900

    Feat: Review First Page

---
+++ src/components/organisms/Community/BestReview/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BestReview = () => {
+   return (
+      <span className='flex justify-center items-center w-[98px] h-[34px] bg-black text-white text-p1 font-medium rounded-xl'>
+         BEST REVIEW
+      </span>
+   );
+};
+
+export default BestReview;
--- src/pages/community/review/index.tsx
+++ src/pages/community/review/index.tsx
@@ -1,4 +1,5 @@
 import CommunityItem from '#components/organisms/Community/CommunityItem';
+import ReviewItem, { ReviewItemProps } from '#components/organisms/Community/ReviewItem';
 import DesktopFooter from '#components/organisms/Footer/Desktop';
 import MobileFooter from '#components/organisms/Footer/Mobile';
 import DesktopNavbar from '#components/organisms/Navbar/Desktop';
@@ -11,6 +12,39 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import React from 'react';
 import useBgStore from 'src/store/useTextColor';
+
+const data: ReviewItemProps[] = [
+   {
+      id: 1,
+      isBest: true,
+      category: 'Forli',
+      star: 5,
+      name: '이름이름',
+      text: `저희집에는 아기가 있고 고양이가 있어서 가장 눈여겨 봤던 것은 스크래치에 쉽게 망가지지 않는 점과 오염이 잘 되지 않는 것이었어요.
+      고양이 키우시는 분들은 아실텐데 소파는 그냥 고양이 큰 스크래처일 뿐이라서 몇개 해먹었기 때문에 망설였었는데 진짜 바닥에서 위, i made it out the gutter.
+      여젼히 더 올라가기 위해 매일 난 또 걸어. 뻔한 swaggin 가끔 철없게도 뵐지라도 어련일들 해낼일을 부정못할 어쩌구 저쩌구 도전 예예예예예예예예예예`,
+   },
+   {
+      id: 2,
+      isBest: true,
+      category: 'Forli',
+      star: 5,
+      name: '이름이름',
+      text: `저희집에는 아기가 있고 고양이가 있어서 가장 눈여겨 봤던 것은 스크래치에 쉽게 망가지지 않는 점과 오염이 잘 되지 않는 것이었어요.
+      고양이 키우시는 분들은 아실텐데 소파는 그냥 고양이 큰 스크래처일 뿐이라서 몇개 해먹었기 때문에 망설였었는데 진짜 바닥에서 위, i made it out the gutter.
+      여젼히 더 올라가기 위해 매일 난 또 걸어. 뻔한 swaggin 가끔 철없게도 뵐지라도 어련일들 해낼일을 부정못할 어쩌구 저쩌구 도전 예예예예예예예예예예`,
+   },
+   {
+      id: 3,
+      isBest: false,
+      category: 'Forli',
+      star: 4,
+      name: '이름이름',
+      text: `저희집에는 아기가 있고 고양이가 있어서 가장 눈여겨 봤던 것은 스크래치에 쉽게 망가지지 않는 점과 오염이 잘 되지 않는 것이었어요.
+      고양이 키우시는 분들은 아실텐데 소파는 그냥 고양이 큰 스크래처일 뿐이라서 몇개 해먹었기 때문에 망설였었는데 진짜 바닥에서 위, i made it out the gutter.
+      여젼히 더 올라가기 위해 매일 난 또 걸어. 뻔한 swaggin 가끔 철없게도 뵐지라도 어련일들 해낼일을 부정못할 어쩌구 저쩌구 도전 예예예예예예예예예예`,
+   },
+];
 
 const Review = () => {
    const { setImageId } = useBgStore();
@@ -47,9 +81,10 @@ const Review = () => {
                      <Tab.Panels className={'column w-[686px]'}>
                         <Tab.Panel className={'space-y-10'}></Tab.Panel>
                         <Tab.Panel className={'space-y-10'}></Tab.Panel>
-                        <Tab.Panel className={'space-y-10'}>
-                           <CommunityItem title={'VIIK'} link='' />
-                           <CommunityItem title={'VIIK'} link='' />
+                        <Tab.Panel className={'grid grid-cols-2 gap-y-10'}>
+                           {data.map(a => (
+                              <ReviewItem {...a} />
+                           ))}
                         </Tab.Panel>
                      </Tab.Panels>
                   </Tab.Group>
