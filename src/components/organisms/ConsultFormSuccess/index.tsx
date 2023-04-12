0ecc41ab23851d0b467c55ba5798dff1588dd10b
emulsion199 <wodnjs471@naver.com>
2023-04-12 13:49:16 +0900

    fix: style error solve

--- src/components/organisms/ConsultFormSuccess/index.tsx
+++ src/components/organisms/ConsultFormSuccess/index.tsx
@@ -1,9 +1,14 @@
 import { PATH } from '#constants/path';
+import { cc } from '#utils/string';
 import Link from 'next/link';
 
-const ConsultFormSuccess = () => {
+interface Props {
+   className?: string;
+}
+const ConsultFormSuccess = (props: Props) => {
+   const { className } = props;
    return (
-      <div className={'column justify-center items-center gap-[86px] w-[360px]'}>
+      <div className={cc(className, 'column justify-center items-center gap-[86px]')}>
          <h1>{'상담 신청 완료'}</h1>
          <Link href={PATH.shop} className={'w-full h-15 button primary flex items-center justify-center'}>
             {'제품 보러가기'}
--- src/pages/styling/index.tsx
+++ src/pages/styling/index.tsx
@@ -3,6 +3,7 @@ import Footer from '#components/organisms/Footer';
 import DesktopNavbar from '#components/organisms/Navbar/Desktop';
 import MobileNavbar from '#components/organisms/Navbar/Mobile';
 import { useMobile } from '#hooks/useMobile';
+import { cc } from '#utils/string';
 import React, { useEffect } from 'react';
 import ConsultFormRegister from 'src/components/organisms/ConsultFormRegister';
 import RootTemplate from 'src/components/Template/RootTemplate';
@@ -41,11 +42,11 @@ const StylingPage = () => {
                   <p className='whitespace-pre-wrap text-center pt-[60px] pb-12'>
                      {'VIIK Styling, 더욱 나에게 맞는 공간을 완성해보세요. \n라이프스타일에 맞는 스타일링을 제안드립니다.'}
                   </p>
-                  <ConsultFormRegister className={'w-[428px] px-6 pb-[214px] '} />
+                  <ConsultFormRegister className={cc('px-6 pb-[214px] ', mobile ? 'w-full' : 'w-[490px]')} />
                </>
             ) : (
                <div className={'w-full min-h-[calc(100vh-80px)] flex items-center justify-center'}>
-                  <ConsultFormSuccess />
+                  <ConsultFormSuccess className={cc('px-6', mobile ? 'w-full' : 'w-[490px]')} />
                </div>
             )}
          </div>
